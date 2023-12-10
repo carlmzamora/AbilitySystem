9c8947d5bf618794a3d014f2781f36bb0659d2e0
carlmzamora <carlmzamora@gmail.com>
2023-12-11 00:42:10 +0800

    Added code that allows setting the spawnCount and coneAngle of ShootAbility.

--- Assets/Game/Code/Scripts/AbilitySystem/ShootAbilityFactory.cs
+++ Assets/Game/Code/Scripts/AbilitySystem/ShootAbilityFactory.cs
@@ -7,12 +7,18 @@ public class ShootAbilityFactory : AbilityFactory<ShootAbilityData, ShootAbility
 
 public class ShootAbility : Ability<ShootAbilityData>
 {
-    private List<Modifier> objectModifierInstances = new();
+    private List<Modifier> spawnModifierInstances = new();
     private IShooting shootingController;
+
+    private int currentSpawnCount;
+    private float currentConeAngle;
 
     public override void Setup(GameObject owner)
     {
         shootingController = owner.GetComponent<IShooting>();
+
+        currentSpawnCount = specialData.spawnCount;
+        currentConeAngle = specialData.coneAngle;
 
         CreateModifierInstancesFromData();
     }
@@ -21,18 +27,27 @@ public class ShootAbility : Ability<ShootAbilityData>
     {
         if (shootingController != null)
         {
-            GameObject spawn = Object.Instantiate(specialData.objectReference, shootingController.ProjectileSpawnpoint.position, shootingController.ProjectileSpawnpoint.rotation);
-
-            IModifierSender modifierSender = spawn.GetComponent<IModifierSender>();
-            if(modifierSender != null)
+            if(currentSpawnCount == 0)
             {
-                foreach(Modifier objectModifierInstance in objectModifierInstances)
-                {
-                    Modifier modifierToSend = objectModifierInstance.Clone();
-
-                    modifierSender.Modifiers.Add(modifierToSend);
-                }
+                Debug.LogError("Shoot ability is spawning nothing!");
             }
+            else if(currentSpawnCount == 1)
+            {
+                GameObject spawn = Object.Instantiate(specialData.spawnReference, shootingController.ProjectileSpawnpoint.position, shootingController.ProjectileSpawnpoint.rotation);
+                ApplySpawnModifiers(spawn);
+            }
+            else if(currentSpawnCount > 1)
+            {
+                int steps = currentSpawnCount - 1;
+                float stepValue = currentConeAngle / steps;
+
+                for (int i = 0; i <= steps; i++)
+                {
+                    GameObject spawn = Object.Instantiate(specialData.spawnReference, shootingController.ProjectileSpawnpoint.position, shootingController.ProjectileSpawnpoint.rotation);
+                    spawn.transform.Rotate(new Vector3(0, (-currentConeAngle * 0.5f) + (i * stepValue), 0));
+                    ApplySpawnModifiers(spawn);
+                }
+            }                               
         }
     }
 
@@ -44,9 +59,23 @@ public class ShootAbility : Ability<ShootAbilityData>
 
     private void CreateModifierInstancesFromData()
     {
-        foreach (ModifierFactory objectModifierFactory in specialData.objectModifiers)
+        foreach (ModifierFactory spawnModifierFactory in specialData.spawnModifiers)
         {
-            objectModifierInstances.Add(objectModifierFactory.GetModifier());
+            spawnModifierInstances.Add(spawnModifierFactory.GetModifier());
+        }
+    }
+
+    private void ApplySpawnModifiers(GameObject spawn)
+    {
+        IModifierSender modifierSender = spawn.GetComponent<IModifierSender>();
+        if (modifierSender != null)
+        {
+            foreach (Modifier spawnModifierInstance in spawnModifierInstances)
+            {
+                Modifier modifierToSend = spawnModifierInstance.Clone();
+
+                modifierSender.Modifiers.Add(modifierToSend);
+            }
         }
     }
 }
@@ -54,8 +83,8 @@ public class ShootAbility : Ability<ShootAbilityData>
 [System.Serializable]
 public class ShootAbilityData : AbilityData
 {
-    public GameObject objectReference;
-    public List<ModifierFactory> objectModifiers;
-    public int objectCount;
-    public float coneAngle;
+    public GameObject spawnReference;
+    public List<ModifierFactory> spawnModifiers;
+    public Attribute_Int spawnCount;
+    public Attribute_Float coneAngle;
 }
